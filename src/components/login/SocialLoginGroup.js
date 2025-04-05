6cd67a7525e6bc8f6ac5a3e56086b11fdc40e2c0
이승준 <127376832+hhee4455@users.noreply.github.com>
2025-04-05 23:14:02 +0900

    Refactor : 소셜 로그인 버튼 최적화

--- src/components/login/SocialLoginGroup.js
+++ src/components/login/SocialLoginGroup.js
@@ -2,30 +2,47 @@ import {FaGithub, FaGoogle } from "react-icons/fa";
 import {RiKakaoTalkFill} from "react-icons/ri";
 import SocialLoginButton from "./SocialLoginButton";
 
+const socialLogins = [
+  {
+    provider: "kakao",
+    bgColor: "bg-[#FEE500]",
+    textColor: "text-black",
+    text: "카카오톡으로 로그인",
+    icon: <RiKakaoTalkFill />,
+  },
+  {
+    provider: "github",
+    bgColor: "bg-black",
+    textColor: "text-white",
+    text: "GitHub으로 로그인",
+    icon: <FaGithub />,
+  },
+  {
+    provider: "google",
+    bgColor: "bg-white border",
+    textColor: "text-black",
+    text: "Google로 계속하기",
+    icon: <FaGoogle />,
+  },
+];
+
 export default function SocialLoginGroup() {
+  const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+
   return (
     <div className="space-y-3">
-      <SocialLoginButton
-        bgColor="bg-[#FEE500]"
-        textColor="text-black"
-        text="카카오톡으로 로그인"
-        icon={<RiKakaoTalkFill />}
-        onClick={() => console.log("카카오 로그인")}
-      />
-      <SocialLoginButton
-        bgColor="bg-black"
-        textColor="text-white"
-        text="GitHub으로 로그인"
-        icon={<FaGithub />}
-        onClick={() => console.log("GitHub 로그인")}
-      />
-      <SocialLoginButton
-        bgColor="bg-white border"
-        textColor="text-black"
-        text="Google로 계속하기"
-        icon={<FaGoogle />}
-        onClick={() => console.log("Google 로그인")}
-      />
+      {socialLogins.map(({ provider, bgColor, textColor, text, icon }) => (
+        <SocialLoginButton
+          key={provider}
+          bgColor={bgColor}
+          textColor={textColor}
+          text={text}
+          icon={icon}
+          onClick={() => {
+            window.location.href = `${baseUrl}/auth/login/${provider}`;
+          }}
+        />
+      ))}
     </div>
   );
 }
